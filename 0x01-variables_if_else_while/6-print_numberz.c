242450a9fa15a5f8856da7696fff44cea9e7d45f
Yahya Esbaiss <yahya.esbaiss.es@gmail.com>
2023-07-14 14:20:22 -0700

    jj

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int code = 48;
+for (; code < 58;)
+{
+putchar(code);
+code++;
+}
+putchar('\n');
+return (0);
+}
