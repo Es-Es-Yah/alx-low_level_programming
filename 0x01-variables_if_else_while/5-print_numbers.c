bc03abc6fd9a1cbd46968b3b5f32226e64a8b71b
Yahya Esbaiss <yahya.esbaiss.es@gmail.com>
2023-07-14 14:05:01 -0700

    jj

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 0;
+for (; a < 10;)
+{
+printf("%d", a);
+a++;
+}
+putchar('\n');
+return (0);
+}
