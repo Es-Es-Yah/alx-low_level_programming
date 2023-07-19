8f25baf64fccdb4b65f89b2d42f54f6f7ea97f43
Es-Es-Yah <134742174+Es-Es-Yah@users.noreply.github.com>
2023-07-19 23:50:36 +0100

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @c: The number that its last digit to be printed
+ *Return: The value of the last digit
+ */
+int print_last_digit(int c)
+{
