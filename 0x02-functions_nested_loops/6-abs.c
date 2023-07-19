a1dd32dd1550ba155ec0a0b75afc7818810f33b6
Es-Es-Yah <134742174+Es-Es-Yah@users.noreply.github.com>
2023-07-19 23:36:21 +0100

    Cr7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: The integer that its abs to be computed
+ *Return: Alway 0.
+ */
+int _abs(int)
+{
+int n;
+_abs(n);
+_putchar("%d\n", n);
+return (0);
+}
