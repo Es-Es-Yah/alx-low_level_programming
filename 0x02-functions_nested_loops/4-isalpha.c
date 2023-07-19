618630f34ee2046120432cb804c62749cfc9cefd
Es-Es-Yah <134742174+Es-Es-Yah@users.noreply.github.com>
2023-07-19 16:26:53 +0100

    Up

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,17 @@
-
+#include "main.h"
+/**
+ * _islalpha - Checks for alphabtic charcter
+ *@c: The character to be checked
+ * Return: 1 for aphabetic character or 0 for anything else
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122) || (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
