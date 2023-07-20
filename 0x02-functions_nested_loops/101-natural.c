26db7a92185da533246ec0139f9fde2601eeb297
Es-Es-Yah <134742174+Es-Es-Yah@users.noreply.github.com>
2023-07-20 03:07:54 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,44 +1,20 @@
 #include <stdio.h>
-#include <unistd.h>
-#include "main.h"
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
-int _putchar(char c)
+int main(void)
 {
-return write(1, &c, 1);
-}
-int main()
-{
-int sum = 0;
-for (int i = 0; i < 1024; i++)
+int i, z = 0;
+while (i < 1024)
 {
 if (i % 3 == 0 || i % 5 == 0)
 {
-sum += i;
+z += i;
 }
+i++;
 }
-int temp_sum = sum;
-int length = 0;
-while (temp_sum != 0)
-{
-temp_sum /= 10;
-length++;
-}
-char buffer[length];
-int index = length - 1;
-while (sum != 0)
-{
-buffer[index] = (sum % 10) + '0';
-sum /= 10;
-index--;
-}
-for (int i = 0; i < length; i++)
-{
-_putchar(buffer[i]);
-}
-_putchar('\n');
-return 0;
+printf('%d\n', d);
+return (0);
 }
