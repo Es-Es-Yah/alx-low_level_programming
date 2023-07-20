a50b961348ff36032d5cfc95ab79100fe80934ed
Es-Es-Yah <134742174+Es-Es-Yah@users.noreply.github.com>
2023-07-20 02:28:21 +0100

    Update 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,12 +22,15 @@ _putchar(' ');
 if (c >= 100)
 {
 _putchar((c / 100) + '0');
-_putchar((c / 10) + %10 + '0');
-_putchar(32);
+_putchar((c / 10) + % 10 + '0');
 }
-_putchar(c + '0');
+else if (c <= 99 && c >= 10)
+{
+_putchar((c / 10) + '0');
 }
+_putchar((c %  10) + '0');
 }
 _putchar('\n');
+}
 }
 }
