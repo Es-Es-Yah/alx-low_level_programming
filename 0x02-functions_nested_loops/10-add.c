2ac26d75a9d6eebca7e6f8fac846ddbb788b734a
Es-Es-Yah <134742174+Es-Es-Yah@users.noreply.github.com>
2023-07-20 01:32:01 +0100

    n+N

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds two integers and returns the result
+ *@n: 1st number
+ *@N: 2nd number
+ * Return: Integer value.
+ */
+int add(int n, int N)
+{
+return (n + N);
+}
