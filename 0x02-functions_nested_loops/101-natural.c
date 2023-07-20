5d18d73ca7a3281ed5ccd2ed7fb5860ea12f7115
Es-Es-Yah <134742174+Es-Es-Yah@users.noreply.github.com>
2023-07-20 02:56:04 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 /**
  * main - check the code.
@@ -8,7 +9,8 @@
 int main()
 {
 int sum = 0;
-for (int i = 0; i < 1024; i++) {
+for (int i = 0; i < 1024; i++)
+{
 if (i % 3 == 0 || i % 5 == 0)
 {
 sum += i;
