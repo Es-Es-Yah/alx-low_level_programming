d5f9d04e152f1b1edd4c8698ba869610e725d7d7
Es-Es-Yah <134742174+Es-Es-Yah@users.noreply.github.com>
2023-07-20 02:46:33 +0100

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+int sum = 0;
+for (int i = 0; i < 1024; i++) {
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+int temp_sum = sum;
+int length = 0;
+while (temp_sum != 0)
+{
+temp_sum /= 10;
+length++;
+}
+char buffer[length];
+int index = length - 1;
+while (sum != 0)
+{
+buffer[index] = (sum % 10) + '0';
+sum /= 10;
+index--;
+}
+for (int i = 0; i < length; i++)
+{
+_putchar(buffer[i]);
+}
+_putchar('\n');
+return 0;
+}
